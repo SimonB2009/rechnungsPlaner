a100ed3f3c190e8bdf85ac3ff2be8601da54538d
SimonB2009 <brandstetter.simon@web.de>
2022-06-17 19:07:37 +0200

    eingeben von der Person die man anschreiben will

--- Program.cs
+++ Program.cs
@@ -6,42 +6,48 @@ namespace HelloWorld
      class Program  {
 
          static double totalSumme = 0;
+         static string name = "niemand";
+         static string date = DateTime.Now.ToString("dd-MM-yyyy"); // heutiges Datum
 
          static void Main(string[] args) {
 
          //Console.OutputEncoding = System.Text.Endcoding.UTF8;
 
-             anschreiben(); // go to the funktion anschreiben and show the epistle.
-             showPrice("Bananen", 3, 3.30); // show the price of the product. 
-             showPrice("Äpfel", 4, 1.35); // show the price of the product. 
-             Console.WriteLine("Gesamsummet: " + totalSumme);
-             Console.WriteLine("Versandkosten: " + versand() + "€");
+            Console.Write("     Name an den du schreiben willst:  ");
+            name = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            
+
+            anschreiben(); // go to the funktion anschreiben and show the epistle.
+            showPrice("Bananen", 3, 3.75); // show the price of the product. 
+            showPrice("Äpfel", 4, 1.35); // show the price of the product. 
+            Console.WriteLine("     Gesamsummet: " + totalSumme);
+            Console.WriteLine("     Versandkosten: " + versand() + "€");
         }
 
          static void anschreiben() {
 
-             string name = "Herr Mayer";
-             string date = DateTime.Now.ToString("dd-MM-yyyy");; // heutiges Datum
-
-             Console.WriteLine("Hallo " + name + ",");
-             Console.WriteLine("");
-             Console.WriteLine("Hiermit sende ich ihre Rechnung vom " + date + ".");
-             Console.WriteLine("");
-             Console.WriteLine("Viele Grüße");
-             Console.WriteLine("Simon Brandstetter");
+            Console.WriteLine("     Hallo " + name + ",");
+            Console.WriteLine("");
+            Console.WriteLine("     Hiermit sende ich ihre Rechnung vom " + date + ".");
+            Console.WriteLine("");
+            Console.WriteLine("     Viele Grüße");
+            Console.WriteLine("     Simon Brandstetter");
          }
 
          static void showPrice(string product, int amount, double price) {
            
-             double total = amount * price;
+            double total = amount * price;
 
-             Console.WriteLine("");
-             Console.WriteLine("Produckt: " + product);
-             Console.WriteLine("Menge: " + amount);
-             Console.WriteLine("Preis: " + price + "€");
-             Console.WriteLine("Summe: " + total + "€");
-             Console.WriteLine("");
-             totalSumme = totalSumme + total; 
+            Console.WriteLine("");
+            Console.WriteLine("     Produckt: " + product);
+            Console.WriteLine("     Menge: " + amount);
+            Console.WriteLine("     Preis: " + price + "€");
+            Console.WriteLine("     Summe: " + total + "€");
+            Console.WriteLine("");
+            totalSumme = totalSumme + total; 
          } 
 
          static double versand() {
